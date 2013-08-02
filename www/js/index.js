ef34da34628b5049aeb772e11924cfd2dc85d3d3
Marco Magnano <roots.marco@gmail.com>
2013-08-02 15:34:09 +0200

    Implemented basic navigation

--- www/js/index.js
+++ www/js/index.js
@@ -6,7 +6,7 @@ function loadRepos() {
     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
         var i, repo;
         $.each(data.repositories, function (i, repo) {
-            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+            $("#allRepos").append("<li><a href='rbe-detail.html?owner=" + repo.username + "&name=" + repo.name + "'>"
             + "<h4>" + repo.name + "</h4>"
             + "<p>" + repo.username + "</p></a></li>");
         });
@@ -14,6 +14,34 @@ function loadRepos() {
     });
 }
 
-function setContent() {
-	$("#allRepos").append("<li><h2>test</h2></li>");
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+$('#reposDetail').live('pageshow', function(event) {
+    var owner = getUrlVars().owner;
+    var name = getUrlVars().name;
+    loadRepoDetail(owner,name);
+});
+
+function loadRepoDetail(owner,name) {
+     $.ajax("https://api.github.com/repos/" + owner + "/" + name).done(function(data) {
+         var repo = data;
+         console.log(data);
+
+         $('#repoName').html("<a href='" + repo.homepage + "'>" + repo.name + "</a>");
+         $('#description').text(repo.description);
+         $('#forks').html("<strong>Forks:</strong> " + repo.forks + "<br><strong>Watchers:</strong> " + repo.watchers);
+
+         $('#avatar').attr('src', repo.owner.avatar_url);
+         $('#ownerName').html("<strong>Owner:</strong> <a href='" + repo.owner.url + "'>" + repo.owner.login + "</a>");
+     });
 }
