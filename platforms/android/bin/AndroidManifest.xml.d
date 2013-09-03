d0133551068bfec897caabc4314229145f947401
Marco Magnano <roots.marco@gmail.com>
2013-09-03 11:18:02 +0200

    Removed builds

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/pinoch/Documents/Lavori/Apps/RBE_01/platforms/android/bin/AndroidManifest.xml : \
-/Users/pinoch/Documents/Lavori/Apps/RBE_01/platforms/android/AndroidManifest.xml \
--- platforms/android/bin/RbeApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/pinoch/Documents/Lavori/Apps/RBE_01/platforms/android/bin/RbeApp-debug-unaligned.apk : \
-/Users/pinoch/Documents/Lavori/Apps/RBE_01/platforms/android/bin/RbeApp.ap_ \
-/Users/pinoch/Documents/Lavori/Apps/RBE_01/platforms/android/bin/classes.dex \
-/Users/pinoch/Documents/Lavori/Apps/RBE_01/platforms/android/libs/cordova-3.0.0.jar \
